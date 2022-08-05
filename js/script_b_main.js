84269aa5a8c0a0a8115bb3be10fa4a328c1d53ad
bonie1004 <bonnie10042@gmil.com>
2022-08-05 19:13:58 +0900

    sen commit

--- js/script_b_main.js
+++ js/script_b_main.js
@@ -14,6 +14,16 @@ $(function () {
     pauseOnHover: false, //마우스 호버시 정지
     pauseOnFocus: false //포커스시 정지
   });
+  //slide2
+  $('.intro_dining .slide2').slick({
+    arrows: false, //화살표
+    dots: false, //인디케이스 해체
+    fade: false, //페이드효과
+    autoplay: true, //자동재생
+    autoplaySpeed: 4000, //재생시간
+    pauseOnHover: false, //마우스 호버시 정지
+    pauseOnFocus: false //포커스시 정지
+  });
 });
 
 
@@ -35,4 +45,34 @@ $(function () {
   });
 });
 
+//패밀리 사이트
+$(function () {
+  $('.txt_wrap .btn_fam').on('click', function () {
+    $('.fam_site_wrap .item_wrap').slideToggle();
+  });
+});
+
+// 햄버거버튼
+$(function () {
+  $('header .menuOpen').on('click', function () {
+    $('header .menuWrap').addClass('on');
+  });
+  $('.menuWrap .close').on('click', function () {
+    $('.menuWrap').removeClass('on');
+  });
+});
+
+// 헤더 이벤트
+var scrollTop = 0;
+scrollTop = $(document).scrollTop();
+
+$(window).on('scroll resize', function () {
+  scrollTop = $(document).scrollTop();
+  fixHeader();
+});
+
+function fixHeader() {
+  if (scrollTop > 200) { $('header').addClass('on') }
+  else { $('header').removeClass('on'); }
+}
 
